8e4df5ecee5e7ac0604671ac78555c5f2db6e9ae
henriquerlz <74163240+henriquerxx2@users.noreply.github.com>
2021-02-01 16:20:43 -0300

    mudança no js do modal

--- Maratona Discover/dev.js
+++ Maratona Discover/dev.js
@@ -1,17 +1,29 @@
-const Modal = {
-    open (){
-        //ABRIR MODAL
-        //ADICIONAR A CLASS ACTIVE AO MODAL
-        document
-        .querySelector('.modal-overlay')
-        .classList.add('active')
-    },
 
-    close() {
-        //FECHAR MODAL
-        //REMOVER A CLASS ACTIVE DO MODAL
-        document.querySelector('.modal-overlay')
-        .classList.remove('active')
-        
-    }
-}
+// PROGRAMAÇÃO ORIENTADA A OBJETOS 
+
+
+// OBJETO MODAL E SEUS METODOS DE ABRIR E FECHAR 
+// OPEN () {}   IRA SELECIONAR O ELEMENTO COM CLASSE MODAL-OVERLAY E APÓS ISSO
+// IRÁ ADICIONAR UMA CLASSE AO ELEMENTO.
+
+// CLOSE () {} IRA SELECIONAR O ELEMENTO COM CLASSE MODAL-OVERLAY E APÓS ISSO
+// IRÁ REMOVAR UMA CLASSE DO ELEMENTO QUE NO CASO É A ACTIVE.
+//const Modal = {
+//    open () {
+//        document.querySelector('.modal-overlay').classList.add('active');
+//    },
+//    close () {
+//        document.querySelector('.modal-overlay').classList.remove('active');
+//    }
+//}
+
+// DESSA OUTRA FORMA APENAS UMA LINHA DE CODIGO É UTILIZADA DEVIDO AO 'TOGGLE' 
+// QUE TROCA CLASSES EXISTENTES
+
+const Modal = {
+    toggle () {
+        document.querySelector('.modal-overlay').classList.toggle('active');
+    },
+};
+
+
